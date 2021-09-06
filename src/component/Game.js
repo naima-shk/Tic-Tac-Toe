2182fc4b4a86326c3cf1ee7061294b01fbd2c57b
naima-shk <naimashaikh550@gmail.com>
2021-09-06 18:05:00 +0500

    'implement-tic-tac-toe-game-using-react'

---
+++ src/component/Game.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Board from './Board';
+
+const Game=()=> {
+
+    const handleClick=() =>{
+        
+    }
+   /* const jumpTo=()=> {
+        
+    }
+   const renderMoves=()=> {
+        
+    }*/
+    return (
+        <Board onClick={handleClick} />
+    )
+    }
+
+export default Game
